64e53b26823cc7e9297ee1c01b062d287a37b02a
M. Price <zaptross@outlook.com>
2022-05-24 08:48:58 +1000

    feat(db): role helper functions

--- internal/pgdb/role.go
+++ internal/pgdb/role.go
@@ -9,3 +9,17 @@ type Role struct {
 	ID        string    `gorm:"primarykey"`
 	Emoji     string
 }
+
+func (db *ReactRolesDatabase) RoleAdd(id string, emoji string) {
+	db.DB.Create(&Role{ID: id, Emoji: emoji})
+}
+
+func (db *ReactRolesDatabase) RoleRemove(id string) {
+	db.DB.Delete(&Role{ID: id})
+}
+
+func (db *ReactRolesDatabase) RoleIsEmojiTaken(emoji string) bool {
+	var role Role
+	db.DB.Where("emoji = ?", emoji).First(&role)
+	return role.ID != ""
+}
