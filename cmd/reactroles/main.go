bd604cd89c80ba523a56459e9d064fc5ee544b88
M. Price <zaptross@outlook.com>
2022-05-25 01:18:47 +1000

    feat(bot): update main to use new features

--- cmd/reactroles/main.go
+++ cmd/reactroles/main.go
@@ -1,155 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"strconv"
-	"strings"
-	"syscall"
+	"log"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/zaptross/reactroles/internal/dgclient"
+	"github.com/zaptross/reactroles/internal/pgdb"
 )
 
-var rolesCache = map[string]string{}
-
 func main() {
-	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
-	if err != nil {
-		panic(err)
-	}
-	println("Bot is now running.")
+	var postgresConfig pgdb.PostgresDbParams
+	pgErr := envconfig.Process("postgres", &postgresConfig)
 
-	roleAssignMessageId := os.Getenv("DISCORD_ROLES_MESSAGE_ID")
-	roleAssignChannelId := os.Getenv("DISCORD_ROLES_CHANNEL_ID")
-
-	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		println(fmt.Sprintf("%s: %s", m.Author.Username, m.Content))
-
-		if m.Content == "!ping" {
-			reactErr := s.MessageReactionAdd(m.ChannelID, m.ID, "🏓")
-
-			if reactErr != nil {
-				println(reactErr.Error())
-			}
-		}
-
-		if strings.Contains(m.Content, "!role") {
-			action, roleName, emojiName := splitRoleCommand(m.Content)
-			defer (func() {
-				if s.ChannelMessageDelete(m.ChannelID, m.ID) != nil {
-					fmt.Printf("Failed to delete message %s:%s\n", m.ChannelID, m.ID)
-				}
-			})()
-
-			if roleName == "" || emojiName == "" || (action != "add" && action != "remove") || (action == "add" && rolesCache[emojiName] != "") || (action == "remove" && rolesCache[emojiName] == "") {
-				s.ChannelMessageSendReply(m.ChannelID, "Usage: !role <add/remove> <role name> <emoji>", m.Reference())
-				return
-			}
-
-			if action == "add" {
-				role, roleCreateErr := s.GuildRoleCreate(m.GuildID)
-
-				if roleCreateErr != nil {
-					s.ChannelMessageSendReply(m.ChannelID, "Error creating role", m.Reference())
-					println(roleCreateErr.Error())
-					return
-				}
-
-				_, editErr := s.GuildRoleEdit(m.GuildID, role.ID, roleName, 0, false, 0, true)
-				if editErr != nil {
-					s.ChannelMessageSendReply(m.ChannelID, "Error editing role", m.Reference())
-					println(editErr.Error())
-					return
-				}
-				rolesCache[emojiName] = role.ID
-
-				reactAddErr := s.MessageReactionAdd(roleAssignChannelId, roleAssignMessageId, emojiName)
-				if reactAddErr != nil {
-					s.ChannelMessageSendReply(m.ChannelID, "Error adding reaction", m.Reference())
-					println(reactAddErr.Error())
-					return
-				}
-			}
-
-			if action == "remove" {
-				if rolesCache[emojiName] == "" {
-					s.ChannelMessageSendReply(m.ChannelID, "No role found for that emoji", m.Reference())
-					return
-				}
-
-				deleteErr := s.GuildRoleDelete(m.GuildID, rolesCache[emojiName])
-				if deleteErr != nil {
-					s.ChannelMessageSendReply(m.ChannelID, "Error deleting role", m.Reference())
-					println(deleteErr.Error())
-					return
-				}
-
-				reactRemoveErr := s.MessageReactionsRemoveEmoji(roleAssignChannelId, roleAssignMessageId, emojiName)
-				if reactRemoveErr != nil {
-					s.ChannelMessageSendReply(m.ChannelID, "Error removing reaction", m.Reference())
-					println(reactRemoveErr.Error())
-					return
-				}
-
-				delete(rolesCache, emojiName)
-			}
-		}
-	})
-
-	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
-		if m.UserID == s.State.User.ID || m.MessageID != roleAssignMessageId || rolesCache[m.Emoji.Name] == "" {
-			return
-		}
-
-		println(fmt.Sprintf("adding %s to %s %s", m.UserID, rolesCache[m.Emoji.Name], m.Emoji.Name))
-
-		roleErr := s.GuildMemberRoleAdd(m.GuildID, m.UserID, rolesCache[m.Emoji.Name])
-
-		if roleErr != nil {
-			println(roleErr.Error())
-		}
-	})
-
-	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionRemove) {
-		if m.UserID == s.State.User.ID || m.MessageID != roleAssignMessageId {
-			return
-		}
-
-		println(fmt.Sprintf("removing %s from %s %s", m.UserID, rolesCache[m.Emoji.Name], m.Emoji.Name))
-
-		roleErr := s.GuildMemberRoleRemove(m.GuildID, m.UserID, rolesCache[m.Emoji.Name])
-
-		if roleErr != nil {
-			println(roleErr.Error())
-		}
-	})
-
-	strconv.ParseInt(os.Getenv("DISCORD_BOT_INTENT"), 10, 64)
-
-	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessageReactions | discordgo.IntentsGuildMessages)
-
-	dg.Open()
-
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	dg.Open()
-}
-
-func splitRoleCommand(command string) (string, string, string) {
-	split := strings.Split(command, " ")
-
-	if len(split) == 1 {
-		return "", "", ""
+	if pgErr != nil {
+		log.Fatal(pgErr.Error())
 	}
 
-	return split[1], split[2], split[3]
+	db := pgdb.GetDatabase(postgresConfig)
+
+	var discordConfig dgclient.DiscordGoClientParams
+
+	dgErr := envconfig.Process("discord", &discordConfig)
+
+	if dgErr != nil {
+		log.Fatal(dgErr.Error())
+	}
+
+	discordConfig.DB = db
+
+	bot := dgclient.GetClient(discordConfig)
+
+	bot.Session.AddHandler(bot.GetOnMessageHandler())
+	bot.Session.AddHandler(bot.GetOnReactionAddHandler())
+	bot.Session.AddHandler(bot.GetOnReactionRemoveHandler())
+
+	bot.Connect()
+
+	bot.Disconnect()
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,24 @@ go 1.18
 require github.com/bwmarrin/discordgo v0.25.0
 
 require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gorm.io/gorm v1.23.4 // indirect
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	gorm.io/driver/postgres v1.3.6
 )
